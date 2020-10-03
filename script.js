bc30865bcc24e0cc3b2b06524e25b2ce8ea4a645
Justin Gabriele <gabrielejustin@yahoo.com>
2020-10-03 13:08:11 -0400

    added html to create table-like interface

---
+++ script.js
@@ -0,0 +1,27 @@
+//task one: 
+  //under header, show current date (use moment.js)
+
+//task two: 
+  //make timeblocks, row, 3 columns for each hour
+
+//task three:
+  // (.setAttribute) use to add class to change color based on time (past, present, future)
+
+//task four:
+  //add (<textarea>) to html for user input. 
+  //figure out how to save user input as a value.(.val?)
+
+//task five:
+  //add a save button on the farthest right column
+
+// THEN the text for that event is saved in local storage
+
+//task six:
+  //figure out how to save text into local storage by clicking save button
+
+// WHEN I refresh the page
+// THEN the saved events persist
+
+//task seven:
+  //figure out how to make sure user text stays saved when the page is refreshed
+
