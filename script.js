dfec69d8c3f69629338afd17769c1a98cc5a0b70
Justin Gabriele <gabrielejustin@yahoo.com>
2020-10-04 15:07:18 -0400

    updated start buttons

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 //task one: DONE
   //under header, show current date (use moment.js)
 var currentDate = moment().format('dddd, MMMM Do YYYY');
-
+var currentTime = moment().format('h:mm:ss a')
+var presentTime = 
 $('#currentDay').text(currentDate);
+
+
 
 //task two: DONE
   //make timeblocks, row, 3 columns for each hour
 
 //task three:
   // (.setAttribute) use to add class to change color based on time (past, present, future)
+function checkTime() {
+  var d = new Date();
+  var currentHour = d.getHours();
+  
+  
+};
 
 //task four:
   //add (<textarea>) to html for user input. 
@@ -17,13 +26,12 @@ $('#currentDay').text(currentDate);
 //task five:
   //add a save button on the farthest right column
 
-// THEN the text for that event is saved in local storage
 
 //task six:
   //figure out how to save text into local storage by clicking save button
-
-// WHEN I refresh the page
-// THEN the saved events persist
+function saveData() {
+  
+}
 
 //task seven:
   //figure out how to make sure user text stays saved when the page is refreshed
