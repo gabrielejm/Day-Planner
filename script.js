0a7c4c026d74c955c520242cb5c73b3f1c589692
Justin Gabriele <gabrielejustin@yahoo.com>
2020-10-06 18:08:21 -0400

    saveBtns sort of work and updated checktime

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 //task one: DONE
   //under header, show current date (use moment.js)
 var currentDate = moment().format('dddd, MMMM Do YYYY, h:mm a');
-var currentHour = moment().format();
+var currentHour = moment().format('HH');
 var presentTime = $('#currentDay').text(currentDate);
-var zeroHour = $('#hour-zero').val(9);
+var zeroHour = $('#hour-zero').val(09);
 var firstHour = $('#hour-one').val(10);
 var secondHour = $('#hour-two').val(11);
 var thirdHour = $('#hour-three').val(12);
@@ -14,6 +14,9 @@ var fifthHour = $('#hour-five').val(14);
 var sixthHour = $('#hour-six').val(15);
 var seventhHour = $('#hour-seven').val(16);
 var eighthHour = $('#hour-eight').val(17);
+var saveBtn = $('.saveBtn');
+
+
 
 var timeBlocks = [zeroHour, firstHour, secondHour, thirdHour, fourthHour, fifthHour, sixthHour, seventhHour, eighthHour]
 
@@ -23,20 +26,26 @@ console.log('currentHour:', currentHour)
 checkTime()
 
 
+
 //task three:
   // (.addclass) use to add class to change color based on time (past, present, future)
 function checkTime() {
   for (var i = 0; i < timeBlocks.length; i++) {
     if (currentHour < timeBlocks[i].val()) {
+      console.log('future', currentHour)
+      console.log('futurevalue', timeBlocks[i].val())
       $(timeBlocks[i]).addClass('future');
     }
 
-    else if (currentHour > timeBlocks[i].val()) {
+    else if (currentHour >= timeBlocks[i].val()) {
+      console.log('past', currentHour)
+      console.log('pastvalue', timeBlocks[i].val())
       $(timeBlocks[i]).addClass('past');
 
     } else {
       $(timeBlocks[i]).addClass('present');
-      
+      console.log('present', currentHour)
+      console.log('presentvalue', timeBlocks[i].val())
     }
     }
 
@@ -49,10 +58,18 @@ function checkTime() {
   //figure out how to save user input as a value(.val?), must ONLY save event on the same line as the save button
   //task six:
   //figure out how to save text into local storage by clicking save button
-function saveData() {
   
-}
+    $('button').on('click', function (){
+      var findInput = $(this).parent().prev().children();
+      localStorage.setItem('event', findInput.val());
+      
+      
+     
+    })
 
+
+  
+    
 
  
   
@@ -62,9 +79,8 @@ function saveData() {
 //task seven:
   //figure out how to make sure user text stays saved when the page is refreshed
   //get value from local storage and put back into text area when page is refreshed.
-function getData () {
-
-}
+function getData () {}
+  
 //optional
   //add clear all local storage
 
